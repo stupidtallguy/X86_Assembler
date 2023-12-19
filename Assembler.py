cf0daeb45444665e62e6c5a560e6169e766d877f
Salar Rahnama <salarrahnama0001@gmail.com>
2023-12-19 23:39:11 +0330

    End Of The Main Project

--- Assembler.py
+++ Assembler.py
@@ -1,7 +1,12 @@
 import time
 import re
 import os
-
+offset = 0
+labels = {}
+to_print = []
+jumps = []
+append_Flag = 0
+# hex_offset = format(offset, f'0{16}X')
 #----------------------------------------------------------------------------------- Functions
 
 def start():
@@ -14,12 +19,6 @@ def start():
 
 # ----------------------------------------------------------------------
 
-
-
-# ----------------------------------------------------------------------
-
-
-# ----------------------------------------------------------------------
 
 def convertFunc(number):
     return (binary_to_hex_dict[number[0][0:4]] + binary_to_hex_dict[number[0][4:8]] + " " +
@@ -44,7 +43,11 @@ def convert16Func(number):
 
 def assemble(instruction, first_arg, second_arg):
     number = []
-    if second_arg is None:
+    global append_Flag
+    if instruction == "JMP":
+        to_print.append(" ")
+        append_Flag = 1
+    elif second_arg is None:
         # Handle cases where second_arg is None
         if first_arg in registers_32bit:
             number.append(instructionOpcode[instruction] + registers_32bit[first_arg])
@@ -109,11 +112,29 @@ def assemble(instruction, first_arg, second_arg):
             number.append(
                 instructionOpcode[instruction] + registers_16bit[first_arg])
             return convert16Func2(number)
+        else :
+            print("invalid Shit Bruh")
+
+# ----------------------------------------------------------------------
+
+
+def calculate_16s_complement(negative_number):
+    # Step 1: Convert the absolute value to hexadecimal
+    hex_representation = hex(abs(negative_number))[2:]
+
+    # Step 2: Calculate the 16's complement
+    complement = ''.join([format(15 - int(digit, 16), 'X')
+                         for digit in hex_representation])
+
+    # Step 3: Add 1 to the result
+    result = format(int(complement, 16) + 1, 'X')
+
+    return result
 
 # ----------------------------------------------------------------------
 
 def assemble_from_file():
-    file_path = input("Please Enter Your File Path To Assemble: ")
+    file_path = "InputTest.txt"
     code = ""
     if os.path.exists(file_path):
         with open(file_path, 'r') as file:
@@ -122,28 +143,67 @@ def assemble_from_file():
         print("File not found. Please check the file path.")
     #Spliting each line and get the instructions and ...
     lines = code.split('\n')
-    offset = 0
-    offsets = {} 
+    global offset
+    global append_Flag
     line_number = 0
     for line in lines:
+        append_Flag = 0
         if line != '':
             line_number += 1
-            number = []
-            component = re.split(r'\s|,\s*', line)
-            instruction = component[0]
-            arg1 = component[1]
-            if len(component) > 2:
-                arg2 = component[2]
-            else: 
-               arg2 = None
-            hex_offset = format(offset, f'0{16}X')
-            s = assemble(instruction,arg1,arg2)
-            increase = len(s.replace(" ","")) // 2
-            print(f"0x{hex_offset} : " + s)
-            offsets[hex_offset] = line_number
-            offset += increase
             
-                
+            component = re.split(r'\s|,\s*', line) 
+            if ":" in line:
+                arg1 = None
+                arg2 = None
+                Label1 = component[0].split(":")[0]
+                labels[Label1] = offset
+                to_print.append("0x000000000000000"+ str(offset - increase) + ": "+"NOTHING")
+
+            else:
+                instruction = component[0].upper()
+                arg1 = component[1]
+                if len(component) > 2:
+                    arg2 = component[2]
+                else: 
+                    arg2 = None
+                s = assemble(instruction,arg1,arg2)
+                if append_Flag == 0 :
+                    to_print.append("0x000000000000000" + str(offset) + ": " + s)
+                    increase = len(s.replace(" ","")) // 2
+                    offset += increase
+                else :
+                    jumps.append([len(to_print)-1,arg1,offset])
+                    offset += 2
+                    continue
+        else :
+            print("The file is Empty BRUHHHH!!")
+
+# ----------------------------------------------------------------------
+def jumpings():
+    for i in jumps:
+        WTF_Jumps(i[0], i[1], i[2])
+
+# ----------------------------------------------------------------------
+
+def WTF_Jumps(index , Label2 , Address ):
+    out = ''
+    label_address = labels[Label2] 
+    Addressing = label_address - (Address + 2)
+    if Addressing > 0 :
+        out += "EB " + str(Addressing)
+        to_print[index] = "0x000000000000000" + str(Address) + ": " + out
+
+    elif Addressing < 0 :
+        Siuuuuuu = calculate_16s_complement(Addressing) 
+        out += "EB " + str(Siuuuuuu)
+        to_print[index] = "0x000000000000000" + str(Address) + ": " + out
+
+
+# ----------------------------------------------------------------------
+
+def printings():
+    for i in to_print:
+        print( i ) 
 
 # ----------------------------------------------------------------------------------- Dictionaries 
 
@@ -168,8 +228,10 @@ registers_32bit_MOD00 = {'[eax]': "000", '[ebx]': "011", '[ecx]': '001', '[edx]'
                          '[esp]': '100', '[ebp]': '101'}
 instructionOpcode = {
     'ADD': '000000', 'SUB': '001010', 'AND': '001000', 'OR': '000010', 'XOR': '001100', 'PUSH': '01010',
-    'POP': '01011', 'INC': '01000', 'DEC': '01001'
+    'POP': '01011', 'INC': '01000', 'DEC': '01001' , 'JMP' : 'EB'
 }
 
 # -----------------------------------------------------------------------------------
 start()
+jumpings()
+printings()
